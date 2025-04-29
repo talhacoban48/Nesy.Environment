d77ada7bc83905fc46c7ff8c43a6b89a6b27540f
talhacoban48 <talha.coban@rastmobile.com>
2025-04-29 10:47:31 +0300

    modified changes

---
+++ mobile_setlocalhost.py
@@ -0,0 +1,24 @@
+import os
+
+
+base = os.getcwd()
+project_folder = os.path.join(base, "projects", "nesy", "NESY.Courier.Mobile")
+
+control = """
+            buildConfigField "String", "BASE_URL", '"https://nesy-arasdx-dev-api.araskargo.com.tr/"'
+            buildConfigField "String", "BASE_URL_HOST", '"nesy-arasdx-dev-api.araskargo.com.tr"'
+"""
+change = f"""
+            buildConfigField "String", "BASE_URL", '"http://10.0.2.2:8080/"'
+            buildConfigField "String", "BASE_URL_HOST", '"10.0.2.2:8080"'
+"""
+
+buildFile = os.path.join(project_folder, "app", "build.gradle")
+buildFileCodes = ""
+with open(buildFile, "r") as gradle:
+    buildFileCodes = gradle.read()
+
+buildFileCodes = buildFileCodes.replace(control, change)
+
+with open(buildFile, "w") as gradle:
+    gradle.write(buildFileCodes)
